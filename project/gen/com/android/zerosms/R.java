c0f9426f8785d93c19fa5f5bc88a78eb20f566e0
virtualabs <virtualabs@gmail.com>
2012-10-12 14:32:42 +0200

    Some other required files
    I forgot to push them previously, so here is the fix.

--- project/gen/com/android/zerosms/R.java
+++ project/gen/com/android/zerosms/R.java
@@ -11,11 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_menu_invite=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int btnSendSMS=0x7f050002;
-        public static final int txtMessage=0x7f050001;
+        public static final int btnContact=0x7f050001;
+        public static final int btnSendSMS=0x7f050003;
+        public static final int txtMessage=0x7f050002;
         public static final int txtPhoneNo=0x7f050000;
     }
     public static final class layout {
